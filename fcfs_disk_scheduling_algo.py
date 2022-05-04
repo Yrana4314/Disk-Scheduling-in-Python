8a00e4d6335f2b564d5ec9a6fcf9635a0be9064e
Yogesh Rana <78715211+Yrana4314@users.noreply.github.com>
2022-05-04 12:32:00 -0400

    Create fcfs_disk_scheduling_algo.py
    initial commit

---
+++ fcfs_disk_scheduling_algo.py
@@ -0,0 +1,8 @@
+''' Thsi FCFS Disk Scheduling algorithm. This program will serve a a disk drive with 5,000 cylinders numbered 0-4,999. 
+This program has 50 requests provided below and serve them accordingly to the FCFS algorithm. 
+The program will be passed the initial position of the disk head (65) as a parameter on the command line and 
+it wil return the the total amount of head movement. '''
+''' Request set: 4078, 153, 2819, 3294, 1433, 211, 1594, 2004, 2335, 2007, 771, 1043, 3950, 2784, 
+1881, 2931, 3599, 1245, 4086, 520, 3901, 2866, 947, 3794, 2353, 3970, 3948, 1815, 4621, 372, 2684,
+3088, 827, 3126, 2083, 584, 4420, 1294, 917, 2881, 3659, 2868, 100, 1581, 4581, 1664, 1001, 1213, 
+3439, 4706, 4869'''
